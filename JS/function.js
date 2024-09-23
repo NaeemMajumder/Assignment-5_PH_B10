c02b8457b0c3ad605f52846d8eea0ad4f7e009fa
Robin Islam <robinislam4@gmail.com>
2024-09-23 19:42:20 +0600

    complete main part

--- JS/function.js
+++ JS/function.js
@@ -38,6 +38,8 @@ function card(button){
         let result = navAmount(card_input.value);
         if(result){
             cardAmount(card_amount, card_input);
+
+            history(card_amount, card_input);
         }
     }else{
         alert("Invalid Dotation Amount!!!");
@@ -50,4 +52,28 @@ function cardAmount(card_amount, card_input){
     let current_amount = card_amount.innerText;
     let total_amount = parseInt(current_amount)+parseInt(card_input.value);
     card_amount.innerText = total_amount;
+}
+
+
+// history function......
+function history(card_amount, card_input){
+    console.log(card_input.parentElement.children[1]);
+
+
+    // collect the title and time
+    let div = document.createElement("div");
+    let h1 = document.createElement("h1");
+    let p = document.createElement("p")
+    h1.innerText = `${card_input.value} Taka is ${card_input.parentElement.children[1].innerText}`
+    p.innerText = `Date: ${new Date()}`;
+
+
+    // append h1 and p to div
+    div.append(h1,p);
+    div.classList.add("history-div");
+
+
+    // append in history
+    let history = document.getElementById("History");
+    history.prepend(div);
 }
